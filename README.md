6ada8c341197eab098a15f599c9b1a2c4f6ce3a9
alex-toy <alexei.80@hotmail.fr>
2023-01-08 10:35:36 +0100

    add client side library

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ If you want something on the web page to be automatically refreshed when new dat
 
 ## Create project
 
-<img src="/pictures/create_project.png" title="create project"  width="800">
+<img src="/pictures/create_project.png" title="create project"  width="500">
+
+Right-click on the project *SignalRSample* and add client side library :
+<img src="/pictures/add_client_side_library.png" title="add client side library"  width="500">
+<img src="/pictures/add_client_side_library2.png" title="add client side library"  width="500">
---
+++ SignalRSample/SignalRSample/wwwroot/js/userCount.js
@@ -0,0 +1,22 @@
+﻿var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+connectionUserCount.on("updateTotalViews", value => {
+    console.log(value)
+    var newCountSpan = document.getElementById("totalwiewscounter");
+    newCountSpan.innerText = value.toString();
+});
+
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+function fulfilled() {
+    console.log("connection successfull");
+    newWindowLoadedOnClient();
+}
+
+function rejected() {
+    console.log("connection rejected")
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
